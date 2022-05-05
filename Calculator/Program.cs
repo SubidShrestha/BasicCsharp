3c08ea835162c4594343f167837560c6224980fe
SubidShrestha <subideminem@gmail.com>
2022-05-05 19:13:01 +0545

    first generics

---
+++ Calculator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            calculate<double> c = new calculate<double>(10.0,15.0);
+            Console.WriteLine(c.add());
+            Console.WriteLine(c.subtract());
+            Console.WriteLine(c.multiply());
+            Console.WriteLine(c.divide());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Calculator/calculate.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Calculator
+{
+    internal class calculate<Type>
+    {
+        private Type data1;
+        private Type data2;
+        private Type data3;
+
+        public calculate(Type data1,Type data2)
+        {
+            this.data1 = data1;
+            this.data2 = data2;
+        }
+
+        public Type add()
+        {
+            data3 = (dynamic)data1 + (dynamic)data2;
+            return (data3);
+        }
+
+        public Type subtract()
+        {
+            data3 = (dynamic)data1 - (dynamic)data2;
+            return (data3);
+        }
+
+        public Type multiply()
+        {
+            data3 = (dynamic)data1 * (dynamic)data2;
+            return (data3);
+        }
+
+        public Type divide()
+        {
+            data3 = (dynamic)data1 / (dynamic)data2;
+            return (data3);
+        }
+    }
+}
